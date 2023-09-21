5bfdfc23f548d2f6b7bb867f9150fbef5f170e29
KVS-JAYANTH <93041010+KVS-JAYANTH@users.noreply.github.com>
2023-09-21 15:51:14 +0530

    Updated pxScript.js

--- pxScript.js
+++ pxScript.js
@@ -4,7 +4,42 @@
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-IAM6BOO0JFWX-2");
 
- 
+function validate()
+{  
+var name=document.loginForm.un.value;  
+var password=document.loginForm.pwd.value;
+if(name=="bhargav@gmail.com" && password=="bhargav")
+{
+    aptrinsic("identify",
+    {
+        "id": name+"$1",
+        "email": name,
+        "firstName": "Bhargav"
+    },
+    {
+        "id":"12",
+        "name":"International Business Machine"
+    });
+    return true;
+}
+if(name=="jayanth@gmail.com" && password=="jayanth")
+{
+    aptrinsic("identify",
+    {
+        "id": name+"$1",
+        "email": name,
+        "firstName": "Jayanth"
+    },
+    {
+        "id":"123",
+        "name":"GS"
+    });
+    return true;
+}
+else{
+    return false;
+}
+}  
 
 
 
